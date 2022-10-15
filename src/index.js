5b2e59b932a5cac3e5a68dcc6f27091ead5f7f06
jayeen28 <jayeen6783@gmail.com>
2022-10-15 20:48:17 +0600

    some comments added

--- src/index.js
+++ src/index.js
@@ -17,16 +17,19 @@ const main = async () => {
             if (!stdout.length) return reject({ message: 'No commits found.' });
             resolve(stdout.split('\n').map(d => JSON.parse(d)))
         }));
+        //prepare report content and make html
         const content = commitInfos.filter(({ message }) => message.split(':').length > 1).map(({ message }) => ({ task: message.split(':')[0], description: message.split(':')[1] }))
         if (!content.length) return console.log('No commits found !');
         const [day, month, dateCount, year] = new Date().toDateString().split(' ');
         const date = `${month} ${dateCount}, ${year}`;
         const html = await writeHtml(date, projectName, content);
         const buffer = new Buffer.alloc(html.length, html.toString(), 'utf-8');
+        //save html 
         fs.writeFileSync(path.resolve() + '/work-report.html', buffer, (error) => {
             if (error) throw new Error(error);
             console.log('Report saved.')
         });
+        //generate pdf with puppeteer.
         await handlePdf();
         console.log(`Work report generated. Can be found here: ${path.resolve()}/work-report-done.pdf`);
     }
